5ba384dc9c497bdb37c5c9c00ae23ca745e15169
macarena28 <Macarenaanza000@gmail.com>
2024-08-21 18:55:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,3 +37,21 @@ fetch(DATA_URL)
     // Manejamos cualquier error que pueda ocurrir durante el fetch
     console.error('Hubo un problema con la operación de fetch:', error);
   });
+const DATA_URL = "json/data.json"
+const container = document.getElementById("container")
+
+function showData(dataArray) {
+  dataArray.forEach(item => {
+    container.innerHTML += `<p>${item.name} ${item.lastname}</p>`
+  })
+}
+
+async function getStudentsFromJson() {
+  const res = await fetch(DATA_URL)
+  const data = await res.json()
+  const { students } = data
+  return students
+}
+
+getStudentsFromJson()
+  .then(students => showData(students))
